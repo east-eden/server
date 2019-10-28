3fc478f2d7e272d2d506f9ef0a4cd92c7aa32523
tao wan <hellodudu86@gmail.com>
2019-10-28 22:12:37 +0800

    initial

---
+++ config/sql/reset.sql
@@ -0,0 +1,19 @@
+drop database if exists `db_yokai`;
+create database `db_yokai` character set utf8mb4;
+
+use `db_yokai`;
+drop table if exists `global`;
+create table global (
+    `id` int(10) not null default '0' comment 'yokai server id',
+    `time_stamp` int(10) not null default '0' comment 'current time',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 collate utf8mb4_general_ci comment='global table';
+
+drop table if exists `client`;
+create table client (
+    `id` int(10) not null default '0' comment 'client id',
+    `name` varchar(32) not null default '' comment 'client name',
+    `last_connect_time` int(10) not null default '0' comment 'last connect time stamp',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 collate utf8mb4_general_ci comment='client';
+
