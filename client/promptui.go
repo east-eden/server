47d50b9a8b7912e2899443bd0bd5cf23c7e42384
tao wan <hellodudu86@gmail.com>
2019-11-27 17:16:33 +0800

    cli interface

--- client/promptui.go
+++ client/promptui.go
@@ -21,20 +21,131 @@ type PromptUI struct {
 }
 
 type Command struct {
-	Number  int
-	Text    string
-	Message *transport.Message
+	Number       int
+	Text         string
+	PageID       int
+	GotoPageID   int
+	Message      *transport.Message
+	InputText    string
+	DefaultInput string
 }
 
-func (c *Command) String() string {
-	return fmt.Sprintf("number:%d, text:%s, Message:%s", c.Number, c.Text, c.Message.Name)
+type CommandPage struct {
+	PageID       int
+	ParentPageID int
+	Cmds         []*Command
+}
+
+var (
+	CmdPages = make(map[int]*CommandPage, 0)
+)
+
+func registerCommand(c *Command) {
+	cmdPage, ok := CmdPages[c.PageID]
+	if !ok {
+		fmt.Println("register command failed:", c)
+		return
+	}
+
+	cmdPage.Cmds = append(cmdPage.Cmds, c)
+}
+
+func registerCommandPage(p *CommandPage) {
+	CmdPages[p.PageID] = p
+}
+
+func initCommandPages() {
+
+	// first level page
+	// page main options
+	registerCommandPage(&CommandPage{PageID: 1, ParentPageID: -1, Cmds: make([]*Command, 0)})
+
+	// seconde level page
+	// page server connection options
+	registerCommandPage(&CommandPage{PageID: 2, ParentPageID: 1, Cmds: make([]*Command, 0)})
+
+	// page role options
+	registerCommandPage(&CommandPage{PageID: 3, ParentPageID: 1, Cmds: make([]*Command, 0)})
+
+	// page hero options
+	registerCommandPage(&CommandPage{PageID: 4, ParentPageID: 1, Cmds: make([]*Command, 0)})
+
+	// page item options
+	registerCommandPage(&CommandPage{PageID: 5, ParentPageID: 1, Cmds: make([]*Command, 0)})
+
+	// page equip options
+	registerCommandPage(&CommandPage{PageID: 6, ParentPageID: 1, Cmds: make([]*Command, 0)})
+
+	// page blade options
+	registerCommandPage(&CommandPage{PageID: 7, ParentPageID: 1, Cmds: make([]*Command, 0)})
+
+	// third level options
+}
+
+func initCommands() {
+	// first level page
+	// 0服务器连接
+	registerCommand(&Command{Number: 0, Text: "服务器连接", PageID: 1, GotoPageID: 2, Message: nil})
+
+	// 1角色管理
+	registerCommand(&Command{Number: 1, Text: "角色管理", PageID: 1, GotoPageID: 3, Message: nil})
+
+	// 2英雄管理
+	registerCommand(&Command{Number: 2, Text: "英雄管理", PageID: 1, GotoPageID: 4, Message: nil})
+
+	// 3物品管理
+	registerCommand(&Command{Number: 3, Text: "物品管理", PageID: 1, GotoPageID: 5,
+		Message: nil})
+
+	// 4装备管理
+	registerCommand(&Command{Number: 4, Text: "装备管理", PageID: 1, GotoPageID: 6, Message: nil})
+
+	// 5异刃管理
+	registerCommand(&Command{Number: 5, Text: "异刃管理", PageID: 1, GotoPageID: 7, Message: nil})
+
+	// second level page
+	// 返回上页
+	registerCommand(&Command{Number: 0, Text: "返回上页", PageID: 2, GotoPageID: 1, Message: nil})
+
+	// 1发送登录
+	registerCommand(&Command{Number: 1, Text: "发送登录", PageID: 2, GotoPageID: -1, InputText: "请输入登录客户端ID和名字，以逗号分隔", DefaultInput: "1, dudu", Message: &transport.Message{
+		Type: transport.BodyProtobuf,
+		Name: "yokai_client.MC_ClientLogon",
+		Body: &pbClient.MC_ClientLogon{},
+	}})
+
+	// 2发送心跳
+	registerCommand(&Command{Number: 2, Text: "发送心跳", PageID: 2, GotoPageID: -1, Message: &transport.Message{
+		Type: transport.BodyProtobuf,
+		Name: "yokai_client.MC_HeartBeat",
+		Body: &pbClient.MC_HeartBeat{},
+	}})
+
+	// 返回上页
+	registerCommand(&Command{Number: 0, Text: "返回上页", PageID: 3, GotoPageID: 1, Message: nil})
+
+	// 1改变经验
+	registerCommand(&Command{Number: 1, Text: "改变经验", PageID: 3, GotoPageID: -1, InputText: "请输入要改变的经验值:", DefaultInput: "120", Message: nil})
+
+	// 2改变等级
+	registerCommand(&Command{Number: 2, Text: "改变等级", PageID: 3, GotoPageID: -1, InputText: "请输入要改变的等级:", DefaultInput: "10", Message: nil})
 }
 
 func NewPromptUI(ctx *cli.Context, client *TcpClient) *PromptUI {
 
+	initCommandPages()
+	initCommands()
+
 	ui := &PromptUI{
 		se: &promptui.Select{
 			Label: "方向键选择操作",
+			Size:  10,
+			Templates: &promptui.SelectTemplates{
+				Label:    "{{ .Text }}?",
+				Active:   "  {{ .Number | cyan }} {{ .Text | cyan }}",
+				Inactive: " {{ .Number | white }} {{ .Text | white }}",
+				Selected: "  {{ .Number | red | cyan }} {{ .Text | red | cyan }}",
+			},
 		},
 		po:        &promptui.Prompt{},
 		tcpClient: client,
@@ -42,44 +153,15 @@ func NewPromptUI(ctx *cli.Context, client *TcpClient) *PromptUI {
 
 	ui.ctx, ui.cancel = context.WithCancel(ctx)
 
-	ui.initCommands()
+	ui.se.Items = CmdPages[1].Cmds
 
 	return ui
 }
 
-func (p *PromptUI) initCommands() {
-	items := make([]*Command, 0)
-
-	// 发送心跳
-	items = append(items, &Command{
-		Number: 0,
-		Text:   "发送心跳消息",
-		Message: &transport.Message{
-			Type: transport.BodyProtobuf,
-			Name: "yokai_client.MC_HeartBeat",
-			Body: &pbClient.MC_HeartBeat{},
-		},
-	})
-
-	// 发送登录消息
-	items = append(items, &Command{
-		Number: 1,
-		Text:   "发送登录消息",
-		Message: &transport.Message{
-			Type: transport.BodyProtobuf,
-			Name: "yokai_client.MC_ClientLogon",
-			Body: &pbClient.MC_ClientLogon{
-				ClientId:   1,
-				ClientName: "dudu",
-			},
-		},
-	})
-
-	p.se.Items = items
-}
-
 func (p *PromptUI) Run() error {
 	for {
+		time.Sleep(time.Second)
+
 		select {
 		case <-p.ctx.Done():
 			logger.Info("prompt ui context done...")
@@ -94,7 +176,7 @@ func (p *PromptUI) Run() error {
 		index, _, err := p.se.Run()
 
 		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
+			fmt.Printf("Select failed %v\n", err)
 			return err
 		}
 
@@ -104,7 +186,35 @@ func (p *PromptUI) Run() error {
 			return nil
 		}
 
-		p.tcpClient.SendMessage(items[index].Message)
+		cmd := items[index]
+
+		// jump to next page
+		if nextPage := cmd.GotoPageID; nextPage != -1 {
+			p.se.Items = CmdPages[nextPage].Cmds
+			continue
+		}
+
+		// wait input
+		if len(cmd.InputText) > 0 {
+			p.po.Label = cmd.InputText
+			p.po.Default = cmd.DefaultInput
+			p.po.Validate = func(s string) error {
+				fmt.Println("input result:", s)
+				return nil
+			}
+
+			result, err := p.po.Run()
+			if err != nil {
+				fmt.Println("prompt run error:", err)
+				continue
+			}
+
+			fmt.Println("input result result = ", result)
+		}
+
+		if cmd.Message != nil {
+			p.tcpClient.SendMessage(cmd.Message)
+		}
 	}
 
 	return nil
