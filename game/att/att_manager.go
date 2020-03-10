9f8e3501ba4201047ea652a0dbf1458e2c9f3947
tao wan <hellodudu86@gmail.com>
2020-03-10 17:42:22 +0800

    att calculate

--- game/att/att_manager.go
+++ game/att/att_manager.go
@@ -2,6 +2,7 @@ package att
 
 import (
 	"github.com/yokaiio/yokai_server/internal/define"
+	"github.com/yokaiio/yokai_server/internal/global"
 )
 
 type AttManager struct {
@@ -10,18 +11,28 @@ type AttManager struct {
 	AttExs [define.AttEx_End]int64 //二级属性
 }
 
-func NewAttManager(owner define.PluginObj, atts [define.Att_End]int64, att_exs [define.AttEx_End]int64) *AttManager {
+func NewAttManager(owner define.PluginObj, attID int32) *AttManager {
 	m := &AttManager{
 		Owner: owner,
 	}
 
-	for k, v := range atts {
-		m.Atts[k] = v
+	attEntry := global.GetAttEntry(attID)
+	if attEntry == nil {
+		return m
 	}
 
-	for k, v := range att_exs {
-		m.AttExs[k] = v
-	}
+	m.Atts[define.Att_Str] = attEntry.Str
+	m.Atts[define.Att_Agl] = attEntry.Agl
+	m.Atts[define.Att_Con] = attEntry.Con
+	m.Atts[define.Att_Int] = attEntry.Int
+	m.Atts[define.Att_AtkSpeed] = attEntry.AtkSpeed
+
+	m.AttExs[define.AttEx_MaxHP] = attEntry.MaxHP
+	m.AttExs[define.AttEx_MaxMP] = attEntry.MaxMP
+	m.AttExs[define.AttEx_Atn] = attEntry.Atn
+	m.AttExs[define.AttEx_Def] = attEntry.Def
+	m.AttExs[define.AttEx_Ats] = attEntry.Ats
+	m.AttExs[define.AttEx_Adf] = attEntry.Adf
 
 	return m
 }
@@ -43,5 +54,14 @@ func (m *AttManager) GetAttExValue(index int32) int64 {
 }
 
 func (m *AttManager) CalcAtt() {
-	// todo calc second att value by first att value
+	for k, _ := range m.AttExs {
+		m.AttExs[k] = 0
+	}
+
+	m.AttExs[define.AttEx_MaxHP] = m.Atts[define.Att_Con] + 10000
+	m.AttExs[define.AttEx_MaxMP] = m.Atts[define.Att_Int] + 1000
+	m.AttExs[define.AttEx_Atn] = m.Atts[define.Att_Str] + m.Atts[define.Att_Agl]
+	m.AttExs[define.AttEx_Def] = m.Atts[define.Att_Con] + 1000
+	m.AttExs[define.AttEx_Ats] = m.Atts[define.Att_Int]
+	m.AttExs[define.AttEx_Adf] = m.Atts[define.Att_Con] + m.Atts[define.Att_Int]
 }
--- internal/define/att_define.go
+++ internal/define/att_define.go
@@ -2,21 +2,25 @@ package define
 
 // 一级属性
 const (
-	Att_Str = iota // 力量
-	Att_Agl        // 敏捷
-	Att_Con        // 耐力
-	Att_Int        // 智力
+	Att_Str      = iota // 力量
+	Att_Agl             // 敏捷
+	Att_Con             // 耐力
+	Att_Int             // 智力
+	Att_AtkSpeed        // 攻击速度
 	Att_End
 )
 
 // 二级属性
 const (
-	AttEx_HP  = iota // 血量
-	AttEx_MP         // 蓝量
-	AttEx_Atn        // 物理攻击力
-	AttEx_Def        // 物理防御力
-	AttEx_Ats        // 魔法攻击力
-	AttEx_Adf        // 魔法防御力
+	AttEx_MaxHP = iota // 血量
+	AttEx_MaxMP        // 蓝量
+	AttEx_Atn          // 物理攻击力
+	AttEx_Def          // 物理防御力
+	AttEx_Ats          // 魔法攻击力
+	AttEx_Adf          // 魔法防御力
+
+	AttEx_CurHP // 当前血量
+	AttEx_CurMP // 当前蓝量
 	AttEx_End
 )
 
@@ -25,15 +29,16 @@ type AttEntry struct {
 	ID   int32  `json:"_id"`
 	Desc string `json:"Desc"`
 
-	Str int32 `json:"Str"`
-	Agl int32 `json:"Agl"`
-	Con int32 `json:"Con"`
-	Int int32 `json:"Int"`
+	Str      int64 `json:"Str"`
+	Agl      int64 `json:"Agl"`
+	Con      int64 `json:"Con"`
+	Int      int64 `json:"Int"`
+	AtkSpeed int64 `json:"AtkSpeed"`
 
-	Hp  int32 `json:"Hp"`
-	Mp  int32 `json:"Mp"`
-	Atn int32 `json:"Atn"`
-	Def int32 `json:"Def"`
-	Ats int32 `json:"Ats"`
-	Adf int32 `json:"Adf"`
+	MaxHP int64 `json:"MaxHP"`
+	MaxMP int64 `json:"MaxMP"`
+	Atn   int64 `json:"Atn"`
+	Def   int64 `json:"Def"`
+	Ats   int64 `json:"Ats"`
+	Adf   int64 `json:"Adf"`
 }
