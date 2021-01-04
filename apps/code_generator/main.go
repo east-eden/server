c3d26bfeba6fb22c3bccb194a86602b2089a9da4
dudu <hellodudu86@gmail.com>
2021-01-04 16:16:27 +0800

    code generate remove auto folder when run

--- apps/code_generator/main.go
+++ apps/code_generator/main.go
@@ -19,7 +19,21 @@ func main() {
 	// logger init
 	logger.InitLogger("code_generator")
 
+	// remove all generated files in previous run
+	dir, err := os.Getwd()
+	if utils.ErrCheck(err, "get working directory failed") {
+		os.Exit(1)
+	}
+	err = os.RemoveAll(fmt.Sprintf("%s/excel/auto/", dir))
+	if utils.ErrCheck(err, "remove all file in config/excel/auto/ failed", dir) {
+		os.Exit(1)
+	}
+
 	// generate go code with excel files
+	err = os.MkdirAll(fmt.Sprintf("%s/excel/auto/", dir), 0777)
+	if utils.ErrCheck(err, "make directory config/excel/auto failed", dir) {
+		os.Exit(1)
+	}
 	excel.Generate("config/excel")
 
 	log.Info().Msg("generate all go code from excel files success!")
