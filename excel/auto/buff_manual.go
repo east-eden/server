135fc8ff4f20be1b8be0c0a54868aefe53ca6092
Tao Wan <tao.wan@centurygame.com>
2021-01-29 15:58:48 +0800

    excel表格的interface{}类型允许字段自定义类型及读取方式

---
+++ excel/auto/buff_manual.go
@@ -0,0 +1,16 @@
+package auto
+
+import "bitbucket.org/east-eden/server/excel"
+
+func init() {
+	excel.AddEntryManualLoader("buff.xlsx", (*BuffEntries)(nil))
+}
+
+// 特殊类型字段处理
+func (e *BuffEntries) ManualLoad(excelFileRaw *excel.ExcelFileRaw) error {
+	rows := GetBuffRows()
+	for _, row := range rows {
+		row.TestField = []int32{1, 2, 3, 4, 5}
+	}
+	return nil
+}
