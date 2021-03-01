973c50bea45e40a59195219d9185843ce7e036d4
Tao Wan <tao.wan@centurygame.com>
2021-03-01 15:27:46 +0800

    git 地址切换

--- excel/auto/heroPromote_entry.go
+++ excel/auto/heroPromote_entry.go
@@ -7,26 +7,26 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var heroPromoteEntries *HeroPromoteEntries //HeroPromote.xlsx全局变量
+var	heroPromoteEntries	*HeroPromoteEntries	//HeroPromote.xlsx全局变量
 
 // HeroPromote.xlsx属性表
 type HeroPromoteEntry struct {
-	Id            int32   `json:"Id,omitempty"`            // 主键
-	PromoteCostId []int32 `json:"PromoteCostId,omitempty"` //突破消耗id
-	PromoteAttId  []int32 `json:"PromoteAttId,omitempty"`  //装备突破属性
+	Id             	int32               	`json:"Id,omitempty"`	// 主键       
+	PromoteCostId  	[]int32             	`json:"PromoteCostId,omitempty"`	//突破消耗id    
+	PromoteAttId   	[]int32             	`json:"PromoteAttId,omitempty"`	//装备突破属性    
 }
 
 // HeroPromote.xlsx属性表集合
 type HeroPromoteEntries struct {
-	Rows map[int32]*HeroPromoteEntry `json:"Rows,omitempty"` //
+	Rows           	map[int32]*HeroPromoteEntry	`json:"Rows,omitempty"`	//          
 }
 
-func init() {
+func  init()  {
 	excel.AddEntryLoader("HeroPromote.xlsx", (*HeroPromoteEntries)(nil))
 }
 
 func (e *HeroPromoteEntries) Load(excelFileRaw *excel.ExcelFileRaw) error {
-
+	
 	heroPromoteEntries = &HeroPromoteEntries{
 		Rows: make(map[int32]*HeroPromoteEntry, 100),
 	}
@@ -38,23 +38,24 @@ func (e *HeroPromoteEntries) Load(excelFileRaw *excel.ExcelFileRaw) error {
 			return err
 		}
 
-		heroPromoteEntries.Rows[entry.Id] = entry
+	 	heroPromoteEntries.Rows[entry.Id] = entry
 	}
 
 	log.Info().Str("excel_file", excelFileRaw.Filename).Msg("excel load success")
 	return nil
-
+	
 }
 
-func GetHeroPromoteEntry(id int32) (*HeroPromoteEntry, bool) {
+func  GetHeroPromoteEntry(id int32) (*HeroPromoteEntry, bool) {
 	entry, ok := heroPromoteEntries.Rows[id]
 	return entry, ok
 }
 
-func GetHeroPromoteSize() int32 {
+func  GetHeroPromoteSize() int32 {
 	return int32(len(heroPromoteEntries.Rows))
 }
 
-func GetHeroPromoteRows() map[int32]*HeroPromoteEntry {
+func  GetHeroPromoteRows() map[int32]*HeroPromoteEntry {
 	return heroPromoteEntries.Rows
 }
+
--- excel/auto/item_entry.go
+++ excel/auto/item_entry.go
@@ -7,36 +7,36 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var itemEntries *ItemEntries //Item.xlsx全局变量
+var	itemEntries    	*ItemEntries   	//Item.xlsx全局变量  
 
 // Item.xlsx属性表
 type ItemEntry struct {
-	Id                 int32   `json:"Id,omitempty"`                 // 主键
-	Type               int32   `json:"Type,omitempty"`               //物品类型
-	SubType            int32   `json:"SubType,omitempty"`            //物品子类型
-	Quality            int32   `json:"Quality,omitempty"`            //品质
-	MaxStack           int32   `json:"MaxStack,omitempty"`           //最大堆叠数
-	TimeLife           int32   `json:"TimeLife,omitempty"`           //时限（分钟）
-	TimeStartLifeStamp int32   `json:"TimeStartLifeStamp,omitempty"` //时限开始时间（unix时间戳）
-	CanSell            bool    `json:"CanSell,omitempty"`            //是否可以出售
-	SellType           int32   `json:"SellType,omitempty"`           //出售货币类型
-	SellPrice          int32   `json:"SellPrice,omitempty"`          //出售价格
-	StaleGainId        int32   `json:"StaleGainId,omitempty"`        //过期后转换的掉落id
-	EffectType         int32   `json:"EffectType,omitempty"`         //使用效果类型
-	EffectValue        []int32 `json:"EffectValue,omitempty"`        //使用效果参数
+	Id             	int32               	`json:"Id,omitempty"`	// 主键       
+	Type           	int32               	`json:"Type,omitempty"`	//物品类型      
+	SubType        	int32               	`json:"SubType,omitempty"`	//物品子类型     
+	Quality        	int32               	`json:"Quality,omitempty"`	//品质        
+	MaxStack       	int32               	`json:"MaxStack,omitempty"`	//最大堆叠数     
+	TimeLife       	int32               	`json:"TimeLife,omitempty"`	//时限（分钟）    
+	TimeStartLifeStamp	int32               	`json:"TimeStartLifeStamp,omitempty"`	//时限开始时间（unix时间戳）
+	CanSell        	bool                	`json:"CanSell,omitempty"`	//是否可以出售    
+	SellType       	int32               	`json:"SellType,omitempty"`	//出售货币类型    
+	SellPrice      	int32               	`json:"SellPrice,omitempty"`	//出售价格      
+	StaleGainId    	int32               	`json:"StaleGainId,omitempty"`	//过期后转换的掉落id
+	EffectType     	int32               	`json:"EffectType,omitempty"`	//使用效果类型    
+	EffectValue    	[]int32             	`json:"EffectValue,omitempty"`	//使用效果参数    
 }
 
 // Item.xlsx属性表集合
 type ItemEntries struct {
-	Rows map[int32]*ItemEntry `json:"Rows,omitempty"` //
+	Rows           	map[int32]*ItemEntry	`json:"Rows,omitempty"`	//          
 }
 
-func init() {
+func  init()  {
 	excel.AddEntryLoader("Item.xlsx", (*ItemEntries)(nil))
 }
 
 func (e *ItemEntries) Load(excelFileRaw *excel.ExcelFileRaw) error {
-
+	
 	itemEntries = &ItemEntries{
 		Rows: make(map[int32]*ItemEntry, 100),
 	}
@@ -48,23 +48,24 @@ func (e *ItemEntries) Load(excelFileRaw *excel.ExcelFileRaw) error {
 			return err
 		}
 
-		itemEntries.Rows[entry.Id] = entry
+	 	itemEntries.Rows[entry.Id] = entry
 	}
 
 	log.Info().Str("excel_file", excelFileRaw.Filename).Msg("excel load success")
 	return nil
-
+	
 }
 
-func GetItemEntry(id int32) (*ItemEntry, bool) {
+func  GetItemEntry(id int32) (*ItemEntry, bool) {
 	entry, ok := itemEntries.Rows[id]
 	return entry, ok
 }
 
-func GetItemSize() int32 {
+func  GetItemSize() int32 {
 	return int32(len(itemEntries.Rows))
 }
 
-func GetItemRows() map[int32]*ItemEntry {
+func  GetItemRows() map[int32]*ItemEntry {
 	return itemEntries.Rows
 }
+
