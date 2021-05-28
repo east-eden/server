1b224d4338bb0aa322139159de7b7d539796d8ff
万韬 <hellodudu86@gmail.com>
2021-05-28 10:52:32 +0800

    洛基id修改

--- services/game/player/player.go
+++ services/game/player/player.go
@@ -380,7 +380,7 @@ func (p *Player) OnFirstLogon() {
 	_ = p.HeroManager().GainLoot(1, 1)
 	_ = p.HeroManager().GainLoot(2, 1)
 	_ = p.HeroManager().GainLoot(3, 1)
-	_ = p.HeroManager().GainLoot(8, 1)
+	_ = p.HeroManager().GainLoot(4, 1)
 	_ = p.HeroManager().GainLoot(100, 1)
 
 	_ = p.TokenManager().GainLoot(define.Token_Strength, 999)
