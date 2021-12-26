c9cbc1c95a099f60d91c7f1c72434afa9fb90b37
dudu <dudu@jiemogame.com>
2021-12-26 09:51:31 +0800

    merge from coding

--- proto/server/pubsub/pubsub.pb.micro.go
+++ proto/server/pubsub/pubsub.pb.micro.go
@@ -14,9 +14,3 @@ import (
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the proto package it is being compiled against.
-// A compilation error at this line likely means your copy of the
-// proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
